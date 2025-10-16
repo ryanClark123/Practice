22e006adec6a5ac79e685ccdd1ba79637e0c320d
ryanClark123 <rcgeneveo@repay.ph>
2025-10-16 11:05:45 +0800

    fix migration

--- database/migrations/2025_10_15_032725_create_posts_table.php
+++ database/migrations/2025_10_15_032725_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('content');
-            $table->foreignId('user_id')->cascade()->onDelete('');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
             $table->softDeletes();
         });
